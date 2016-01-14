0994499a32426f4c32be4c275f3aef6e18432bbd
vagrant <vagrant@ADHost.(none)>
2016-01-14 10:14:12 +0000

    fix setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DOT_FILES=( .inputrc .vimrc .vimrc.bundle .vimrc.plugins_setting .perltidyrc .tm
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/rc/$file $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
